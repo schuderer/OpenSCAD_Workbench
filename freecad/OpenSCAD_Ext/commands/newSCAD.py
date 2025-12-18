211c24b9c0006c0927e7a0be4ddf9037d6f413fd
Keith Sloan <keithsloan52@icloud.com>
2025-12-18 16:49:53 +0000

    Stack

--- freecad/OpenSCAD_Ext/commands/newSCAD.py
+++ freecad/OpenSCAD_Ext/commands/newSCAD.py
@@ -6,55 +6,127 @@ from PySide import QtGui
 
 from freecad.OpenSCAD_Ext.logger.Workbench_logger import write_log
 
-class OpenSCADeditOptions(QtGui.QDialog):
-        def __init__(self,parent=None):
-                super(OpenSCADeditOptions, self).__init__(parent)
-                self.initUI()
+class SCADFileNameValue(QtGui.QWidget):
+    def __init__(self, label="SCADFileName", default="", parent=None, fileFilter="*.scad"):
+        super(SCADFileNameValue, self).__init__(parent)
 
-        def initUI(self):
-                self.result = None
-                # create our window
-                # define window           xLoc,yLoc,xDim,yDim
-                self.setGeometry(150, 250, 300, 300)
-                self.setWindowTitle("FC OpenSCAD edit Options")
-                self.layout = QtGui.QVBoxLayout()
-                self.setMouseTracking(True)
-                self.buttonBox = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel, self)
+        self.fileFilter = fileFilter
 
-                # Connect the dialog buttons to standard slots
-                self.buttonBox.accepted.connect(self.accept)
-                self.buttonBox.rejected.connect(self.reject)
-                self.createOnly = BooleanValue('Create Only (Edit)',True)
-                self.layout.addWidget(self.createOnly)
-                self.importType = ImportType()
+        layout = QtGui.QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        self.label = QtGui.QLabel(label)
+        layout.addWidget(self.label)
+
+        self.lineEdit = QtGui.QLineEdit(default)
+        self.lineEdit.setPlaceholderText("Enter filename")
+        layout.addWidget(self.lineEdit, 1)
+
+class GeometryType(QtGui.QWidget):
+        def __init__(self):
+                super().__init__()
+                self.layout = QtGui.QHBoxLayout()
+                self.label = QtGui.QLabel('Geometry Type')
+                self.layout.addWidget(self.label)
+                self.importType = QtGui.QComboBox()
+                self.importType.addItem('Mesh')
+                self.importType.addItem('Brep')
+                self.importType.addItem('Opt')
                 self.layout.addWidget(self.importType)
-                self.layout.addWidget(self.importType)
-                self.fnMax = IntegerValue('FnMax', 16)
-                self.layout.addWidget(self.fnMax)
-                self.timeOut = IntegerValue('TimeOut', 30)
-                self.layout.addWidget(self.timeOut)
-                self.keepOption = BooleanValue("Keep File",False)
-                self.layout.addWidget(self.keepOption)
-                self.layout.addWidget(self.buttonBox)
                 self.setLayout(self.layout)
-                self.show()
 
-        def getValues(self):
-                return(
-                        self.createOnly.getVal(), \
-                        self.importType.getVal(), \
-                        self.fnMax.getVal(), \
-                        self.timeOut.getVal(), \
-                        self.keepOption.getVal()
-                        )
+        def getVal(self):
+                return self.importType.currentText()
+                     
 
-        def onCancel(self):
-                self.result = 'cancel'
-                #QtGui.QGuiApplication.restoreOverrideCursor()
+class IntegerValue(QtGui.QWidget):
+	def __init__(self, label, value):
+		super().__init__()
+		self.layout = QtGui.QHBoxLayout()
+		self.label = QtGui.QLabel(label)
+		self.value = QtGui.QLineEdit()
+		self.value.setText(str(value))
+		self.layout.addWidget(self.label)
+		self.layout.addWidget(self.value)
+		self.setLayout(self.layout)
 
-        def onOk(self):
-                self.result = 'ok'
-                #QtGui.QGuiApplication.restoreOverrideCursor()
+	def getVal(self):
+		return int(self.value.text())
+
+class BooleanValue(QtGui.QWidget):
+	def __init__(self, label, value):
+		super().__init__()
+		self.layout = QtGui.QHBoxLayout()
+		self.label = QtGui.QLabel(label)
+		self.value = QtGui.QRadioButton()
+		self.value.setChecked(value)
+		self.layout.addWidget(self.label)
+		self.layout.addWidget(self.value)
+		self.setLayout(self.layout)
+
+	def getVal(self):
+		if self.value.isChecked():
+			return True
+		else:
+			return False
+
+class OpenSCADeditOptions(QtGui.QDialog):
+    def __init__(self, parent=None, filename=""):
+        super(OpenSCADeditOptions, self).__init__(parent)
+        self.filename = filename
+        self.initUI()
+
+    def initUI(self):
+        self.result = None
+
+        self.setGeometry(150, 250, 400, 300)
+        self.setWindowTitle("SCAD File Object edit Options")
+
+        self.layout = QtGui.QVBoxLayout()
+        self.setMouseTracking(True)
+
+        # ---------- Options ----------
+        self.scadFileName = SCADFileNameValue()
+        self.layout.addWidget(self.scadFileName)
+        self.geometryType = GeometryType()
+        self.layout.addWidget(self.geometryType)
+        self.fnMax = IntegerValue("FnMax", 16)
+        self.layout.addWidget(self.fnMax)
+        self.timeOut = IntegerValue("TimeOut", 30)
+        self.layout.addWidget(self.timeOut)
+        self.keepOption = BooleanValue("Keep File", False)
+        self.layout.addWidget(self.keepOption)
+
+        # ---------- OK / Cancel ----------
+        self.buttonBox = QtGui.QDialogButtonBox(
+            QtGui.QDialogButtonBox.Ok | QtGui.QDialogButtonBox.Cancel,
+            self
+        )
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+        self.show()
+
+
+    def getValues(self):
+        return(
+              self.scadFileName.getVal(), \
+              self.geometry.getVal(), \
+              self.fnMax.getVal(), \
+              self.timeOut.getVal(), \
+              self.keepOption.getVal()
+              )
+
+    def onCancel(self):
+        self.result = 'cancel'
+        #QtGui.QGuiApplication.restoreOverrideCursor()
+
+    def onOk(self):
+        self.result = 'ok'
+        #QtGui.QGuiApplication.restoreOverrideCursor()
 
 class NewSCADFile_Class:
     """Create a new SCAD file Object """
